af25ea84dad08da2bb4ff8db3d44dbd0ec0b840e
Loic-design <nativel.loic95@gmail.com>
2026-09-16 15:25:15 +0200

    responsive accueil

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,18 +9,45 @@ export default function Home() {
   const [menuOuvert, setMenuOuvert] = useState(false)
 
   return (
-    <main className="w-full max-w-[1200px] h-[900px] mx-auto flex items-center justify-center overflow-hidden">
-      {!menuOuvert ? <img className="" onClick={() => setMenuOuvert(true)} src="/medias/Logo_Top_Flop.png" alt="Logo" /> :
-        <div className=" bg-white rounded-2xl shadow-lg">
-          <img className="w-250" src="/medias/Logo_Top_Flop.png" alt="" />
-          <div className="flex justify-center gap-20 mb-20">
-            <Link className="px-6 py-3  text-black rounded-xl font-semibold hover:bg-green-500 transition"
-              href="/creer-salon"> Créer un salon</Link>
-            <Link className="px-6 py-3  text-black rounded-xl font-semibold hover:bg-red-500 transition"
-              href="/rejoindre-salon"> Rejoindre un salon </Link>
-          </div>
-        </div>}
+    <main className="w-full max-w-[1200px] min-h-screen mx-auto px-4 md:px-6 lg:px-8 flex items-center justify-center overflow-hidden">
+      {!menuOuvert ? (
+        <div className="flex flex-col items-center justify-center gap-4 max-h-[80vh]">
+          <img
+            className="w-[70%] max-w-[280px] md:w-[80%] md:max-w-none mx-auto p-8 animate-pulse cursor-pointer"
+            onClick={() => setMenuOuvert(true)}
+            src="/medias/Logo_Top_Flop.png"
+            alt="Logo Top Flop"
+          />
 
+          <p className="text-sm text-gray-500">
+            Appuyez pour commencer
+          </p>
+        </div>
+      ) : (
+        <div className="bg-white rounded-2xl shadow-lg w-full md:w-1/2 max-h-[80vh] p-8">
+          <img
+            className="w-[70%] max-w-[280px] mx-auto"
+            src="/medias/Logo_Top_Flop.png"
+            alt="Logo Top Flop"
+          />
+
+          <div className="flex flex-col md:flex-row items-center justify-center gap-6 mt-8">
+            <Link
+              className="px-6 py-3 text-black rounded-xl font-semibold hover:bg-green-500 transition"
+              href="/creer-salon"
+            >
+              Créer un salon
+            </Link>
+
+            <Link
+              className="px-6 py-3 text-black rounded-xl font-semibold hover:bg-red-500 transition"
+              href="/rejoindre-salon"
+            >
+              Rejoindre un salon
+            </Link>
+          </div>
+        </div>
+      )}
     </main>
   );
 
